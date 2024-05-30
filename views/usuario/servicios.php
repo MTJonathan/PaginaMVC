6f6bd91822de6da482ed0e400de13d41a76a1edb
MTJonathan <jonathan02330@gmail.com>
2024-05-30 14:41:21 -0500

    Servicios Fase 02

--- views/usuario/servicios.php
+++ views/usuario/servicios.php
@@ -1,33 +1,33 @@
-<h1>Servicios :D</h1>
+<h1 class="h1-servicios">Servicios :D</h1>
 <div class="contenedor-principal">
     <div class="contenedor">
-        <h2>Servicio 01</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+        <h2 class="h2-servicios">Servicio 01</h2>
+        <p class="p-servicios">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
             exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
     </div>
     <div class="contenedor">
-        <h2>Servicio 02</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+        <h2 class="h2-servicios">Servicio 02</h2>
+        <p class="p-servicios">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
             exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
     </div>
     <div class="contenedor">
-        <h2>Servicio 03</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+        <h2 class="h2-servicios">Servicio 03</h2>
+        <p class="p-servicios">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
             exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
     </div>
     <div class="contenedor">
-        <h2>Servicio 04</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+        <h2 class="h2-servicios">Servicio 04</h2>
+        <p class="p-servicios">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
             exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
     </div>
     <div class="contenedor">
-        <h2>Servicio 05</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+        <h2 class="h2-servicios">Servicio 05</h2>
+        <p class="p-servicios">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
             exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
     </div>
     <div class="contenedor">
-        <h2>Servicios 06</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+        <h2 class="h2-servicios">Servicios 06</h2>
+        <p class="p-servicios">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
             exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
     </div>
     
