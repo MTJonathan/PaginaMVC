de5d3b790f3bf70c23c38df5db0722b9c777c14f
MTJonathan <jonathan02330@gmail.com>
2024-05-30 12:45:53 -0500

    Servicios Fase 01

--- views/usuario/servicios.php
+++ views/usuario/servicios.php
@@ -1,5 +1,34 @@
+<h1>Servicios :D</h1>
 <div class="contenedor-principal">
-    <h1>Servicios :D</h1>
-    <div class="contenedor"> aa</div>
-    <div class="contenedor"> aa</div>
+    <div class="contenedor">
+        <h2>Servicio 01</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+            exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
+    </div>
+    <div class="contenedor">
+        <h2>Servicio 02</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+            exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
+    </div>
+    <div class="contenedor">
+        <h2>Servicio 03</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+            exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
+    </div>
+    <div class="contenedor">
+        <h2>Servicio 04</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+            exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
+    </div>
+    <div class="contenedor">
+        <h2>Servicio 05</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+            exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
+    </div>
+    <div class="contenedor">
+        <h2>Servicios 06</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aspernatur voluptas possimus sunt quae reprehenderit dolor, 
+            exercitationem maiores necessitatibus natus, officia at blanditiis dicta? Reprehenderit magnam recusandae quam praesentium. Alias.</p>
+    </div>
+    
 </div>
