b2f37f696e9adf1077a634e1f3e9d3232f3e1628
MTJonathan <jonathan02330@gmail.com>
2024-05-30 21:20:06 -0500

    Terminado creo xs

--- views/header.php
+++ views/header.php
@@ -8,13 +8,14 @@
     <link rel="stylesheet" href="<?= $GLOBALS['ruta_raiz']; ?>/public/css/editar.css">
     <link rel="stylesheet" href="<?= $GLOBALS['ruta_raiz']; ?>/public/css/inicio.css">
     <link rel="stylesheet" href="<?= $GLOBALS['ruta_raiz']; ?>/public/css/servicios.css">
+    <link rel="stylesheet" href="<?= $GLOBALS['ruta_raiz']; ?>/public/css/ayuda.css">
     <title>Trabajo Final</title>
 </head>
 <header>
     <nav class="menu-principal">
         <a href="<?= $GLOBALS['ruta_raiz']; ?>/inicio" class="enlace" data-pagina="inicio">Inicio</a>
         <a href="<?= $GLOBALS['ruta_raiz']; ?>/servicios" class="enlace">Servicios</a>
-        <a href="#" class="enlace">Centro de Ayuda</a>
+        <a href="<?= $GLOBALS['ruta_raiz']; ?>/centro/ayuda" class="enlace">Centro de Ayuda</a>
         <a href="<?= $GLOBALS['ruta_raiz']; ?>/usuario/editar" class="enlace" data-pagina="miUsuario">Mi Usuario</a>
     </nav>
 </header>
---
+++ views/usuario/ayuda.php
@@ -0,0 +1,34 @@
+    <h1 class="h1-ayuda">Centro De Ayuda =)</h1>
+    <div class="preguntas">
+        <h2>Preguntas Frecuentes</h2>
+        <details>
+            <summary>¿Donde se encuentran?</summary>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima ex totam obcaecati eos omnis ullam cum, 
+                voluptate magnam fugit, incidunt accusantium non nobis a amet qui corrupti saepe nostrum?</p>
+        </details>
+        <details>
+            <summary>¿A que se Dedican?</summary>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima ex totam obcaecati eos omnis ullam cum, 
+                voluptate magnam fugit, incidunt accusantium non nobis a amet qui corrupti saepe nostrum?</p>
+        </details>
+        <details>
+            <summary>¿Con que precios cuentan?</summary>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima ex totam obcaecati eos omnis ullam cum, 
+                voluptate magnam fugit, incidunt accusantium non nobis a amet qui corrupti saepe nostrum?</p>
+        </details>
+        <details>
+            <summary>¿Cuentan con experiencia?</summary>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima ex totam obcaecati eos omnis ullam cum, 
+                voluptate magnam fugit, incidunt accusantium non nobis a amet qui corrupti saepe nostrum?</p>
+        </details>
+    </div>
+    <div class="img">
+        <img  src="<?= $GLOBALS['ruta_raiz']; ?>/public/img/ayuda.jpg" alt="">
+    </div>
+    <div class="preguntas">
+        <h2>Sobre la empresa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis voluptatum odio repellendus quis quos quo 
+            dolores expedita maiores quas autem culpa fugit explicabo necessitatibus natus, cumque quae neque impedit. Saepe? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis necessitatibus, error mollitia quis architecto aliquid? Repellendus tenetur voluptates eligendi facere illum. Animi ipsa facilis officiis, veniam doloribus minus dolor!</p>
+    </div>
+    
